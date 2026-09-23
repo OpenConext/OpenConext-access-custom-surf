a9d3eb878ebcc8ae1159f63ec7a2f16197e97848
Ricardo van der Heijden <20791917+ricardovdheijden@users.noreply.github.com>
2026-09-23 16:39:46 +0200

    #89 Replaces sample logo with SURF logo

--- client/myCustomApp.tsx
+++ client/myCustomApp.tsx
@@ -1,8 +1,6 @@
 import {AppCustomization} from "../../contexts/CustomizationContext";
-import WfoLogo from "./WfoLogo.svg";
-import LogoSurf from "../../client/src/icons/logo-surf.svg"
+import Logo from "./logo-surf.svg";
 
 export const appCustomization: AppCustomization  = {
-    logoPublic: <WfoLogo />,
-    logoLoggedIn: <LogoSurf />
+    appLogo: <Logo />,
 }
